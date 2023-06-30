ddaf6208bf1e33134cb7bf036966f8e73ec3f1a3
Lucio <kingdomlink69@gmail.com>
2023-06-29 21:12:04 -0300

    GET e POST funcionais

--- src/main/java/com/mvdb/movieDB/controller/MovieController.java
+++ src/main/java/com/mvdb/movieDB/controller/MovieController.java
@@ -1,14 +1,33 @@
 package com.mvdb.movieDB.controller;
 
+import com.mvdb.movieDB.dtos.MovieRecordDto;
 import com.mvdb.movieDB.repositories.MovieRepository;
+import jakarta.validation.Valid;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import com.mvdb.movieDB.models.MovieModel;
+import java.util.List;
 //Controlador da API REST
 @RestController
 public class MovieController {
     @Autowired
     MovieRepository movieRepository;
-    
 
+    @PostMapping("/movies")
+    public ResponseEntity<MovieModel> saveMovie(@RequestBody @Valid MovieRecordDto movieRecordDto){
+        var movieModel = new MovieModel();
+        BeanUtils.copyProperties(movieRecordDto, movieModel);
+        return ResponseEntity.status(HttpStatus.CREATED).body(movieRepository.save(movieModel));
+    }
+    @GetMapping("/movies")
+    public ResponseEntity<List<MovieModel>> getAllMovies(){
+        return ResponseEntity.status(HttpStatus.OK).body(movieRepository.findAll());
+    }
 
 }
--- src/main/java/com/mvdb/movieDB/dtos/MovieRecordDto.java
+++ src/main/java/com/mvdb/movieDB/dtos/MovieRecordDto.java
@@ -1,5 +1,9 @@
 package com.mvdb.movieDB.dtos;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
-public record MovieRecordDto() {
-
+import java.math.BigDecimal;
+//Records são imutáveis
+public record MovieRecordDto( /* Rows do Banco de Dados -> */ @NotBlank String name, @NotNull BigDecimal value) {
+    //Validação dos dados
 }
